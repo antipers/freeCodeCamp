8d427f71f79ed37b8fb456dc08d63844a7befc2c
antipers <antipers@gmail.com>
2024-02-04 03:45:37 +0300

    started lern js

---
+++ js/hw_02/variab.js
@@ -0,0 +1,15 @@
+ 
+// let admin;
+// let name;
+// name = 'Джон'
+
+// admin = name;
+// alert(admin);
+
+// let ourplanet = 'Земля'
+// let CurrentUserName= 'Denis';
+
+"use strict";
+let name = prompt('What is your name?', "Denis");
+
+alert(`Hello, ${name}. Are you OK?`);
